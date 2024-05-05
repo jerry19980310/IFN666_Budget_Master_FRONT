3907ddc4de12b7e4b36b4fe61d1d349bbe836506
jerry19980310 <a0989718985@gmail.com>
2024-05-05 15:31:21 +1000

    API POST (create new categories)

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,65 +1,81 @@
 import { Text, Switch, View, StyleSheet } from "react-native";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { GlobalLayout } from "../components/Layout";
 import { Box, Center, Heading, ScrollView, Stack, VStack, HStack, AspectRatio, Image, Button, Icon, IconButton } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from '@expo/vector-icons';
 
-
-
-
-
 export default function CategoryScreen() {
+  const [dataCategory, setDataCategories] = useState([]);
+  const [isCreate, setIsCreate] = useState(false);
+
+  const createCategory = async () => {
+    try {
+      const response = await axios.post('http://10.0.2.2:3000/api/category/create', {
+        name: 'Testttt',
+        user_id: '1'
+      });
+      console.log(response.data);
+      fetchCategory();
+      setIsCreate(false);
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  };
+
+  const fetchCategory = async () => {
+    try {
+      const category = await axios.get(`http://10.0.2.2:3000/api/category/1`);
+      console.log("category.data: ", category.data);
+      setDataCategories(category.data.categories);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      alert("No image on that date. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    fetchCategory();
+  }, []);
+
+  useEffect(() => {
+
+    if(isCreate === true){
+      createCategory();
+    }
+
+  }, [isCreate]);
 
 
-  const data_Category = [
-    {
-      id: '1',
-      title: 'Transport',
-    },
-    {
-      id: '2',
-      title: 'Food',
-    },
-    {
-      id: '3',
-      title: 'Education',
-    },
-    {
-      id: '4',
-      title: 'Work out',
-    },
-  ];
+  return (
+    <Center>
+      {/* <Heading size="md">column</Heading> */}
+      <ScrollView>
+      {dataCategory.map((item) => (
+        <VStack display="flex" key={item.id} mb="2.5" mt="1.5" direction="column" space={1}>
+          <Center display="flex" size="16" w="300px" h="50px" bg="primary.400" rounded="md" _text={{
+            color: 'warmGray.50',
+            fontWeight: 'medium'
+          }} shadow={'5'}>
+            <HStack>
+              <Text>{item.name}</Text>
+              <IconButton icon={<Icon as={AntDesign} name="edit" size="sm" />} />
+              <IconButton icon={<Icon as={AntDesign} name="delete" size="sm" />} />
+            </HStack>
+          </Center>
+        </VStack>
+      ))}
+      <Button key={1} leftIcon={<Icon as={Ionicons} name="add-circle-outline" size="sm" />} onPress={() => setIsCreate(true) } >Create</Button>
+      </ScrollView>
+      
+    </Center>
+  );
+}
 
-    return (
-      <Center>
-        {/* <Heading size="md">column</Heading> */}
-        {data_Category.map((item) => (
-          <VStack display="flex" key={item.id} mb="2.5" mt="1.5" direction="column" space={1}>
-            <Center display="flex" size="16" w="300px" h="50px" bg="primary.400" rounded="md" _text={{
-              color: 'warmGray.50',
-              fontWeight: 'medium'
-            }} shadow={'5'}>
-              <HStack>
-                <Text>{item.title}</Text>
-                <IconButton icon={<Icon as={AntDesign} name="edit" size="sm" />} />
-                <IconButton icon={<Icon as={AntDesign} name="delete" size="sm" />} />
-              </HStack>
-            </Center>
-            
-            
-
-          </VStack>
-        ))}
-        <Button leftIcon={<Icon as={Ionicons} name="add-circle-outline" size="sm" />}>Create</Button>
-      </Center>
-        
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    view: {
-      flexDirection: "row",
-      alignItems: "center",
-    },
-  });
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
